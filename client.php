e62766806c3a181c7ff277d884897cbce65ffeb0
root <harsh250895@gmail.com>
2018-02-17 20:30:42 -0500

    it490 files

---
+++ client.php
@@ -0,0 +1,43 @@
+
+<?php
+error_reporting(-1);
+ini_set('display_errors', false);
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+function connection($user,$pass){
+    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    if (isset($argv[1]))
+    {
+      $msg = $argv[1];
+    }
+    else
+    {
+      $msg = "test message";
+    }
+
+
+
+    $request = array();
+    $request['type'] = "login";
+    $request['username'] = $user;
+    $request['password'] = $pass;
+    $request['message'] = $msg;
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+
+    //echo "client received response: ".PHP_EOL;
+    //print_r($response);
+    return $response;
+    echo "\n\n";
+
+    echo $argv[0]." END".PHP_EOL;
+}
+
+
+
+
+
+?>

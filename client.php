ad3a7856f8fcd71837e211b217a52b4019152f7e
root <harsh250895@gmail.com>
2018-03-06 20:52:56 -0500

    added locationSearch function and changed server name

--- client.php
+++ client.php
@@ -8,7 +8,7 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 function login($user,$pass){
-    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    $client = new rabbitMQClient("testRabbitMQ.ini","DBServer");
     if (isset($argv[1]))
     {
       $msg = $argv[1];
@@ -37,7 +37,7 @@ function login($user,$pass){
 }
 
 function register($email,$user,$pass){
-    $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+    $client = new rabbitMQClient("testRabbitMQ.ini","DBServer");
     if (isset($argv[1]))
     {
       $msg = $argv[1];
@@ -66,7 +66,32 @@ function register($email,$user,$pass){
     echo $argv[0]." END".PHP_EOL;
 }
 
+function searchLocation($location){
+    $client = new rabbitMQClient("testRabbitMQ.ini","DBServer");
+    if (isset($argv[1]))
+    {
+      $msg = $argv[1];
+    }
+    else
+    {
+      $msg = "test message";
+    }
 
+
+
+    $request = array();
+    $request['type'] = "location";
+    $request['location'] = $location;
+    $response = $client->send_request($request);
+    //$response = $client->publish($request);
+
+    //echo "client received response: ".PHP_EOL;
+    //print_r($response);
+    return $response;
+    echo "\n\n";
+
+    echo $argv[0]." END".PHP_EOL;
+}
 
 
 
